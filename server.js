bbf4358ba29c288b7412b9fba794a0dd57f64db6
zabarullah <60830173+zabarullah@users.noreply.github.com>
2021-12-17 17:54:24 +0000

    changed response on homepage endpoint

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const sqldatabase = knex({
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send(database.users) })
+app.get('/', (req, res) => { res.send('it is working!') })
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, sqldatabase, bcrypt) })
 app.post('/register', (req, res) => { register.handleRegister(req, res, sqldatabase, bcrypt) } )
 app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, sqldatabase) })
