9cfca1415bda3ad6b9bff2cb4cb136ef4bd0e427
Work <zabar_ullah@hotmail.com>
2023-01-13 19:33:37 +0000

    changing model and hard coding apikey

--- controllers/image.js
+++ controllers/image.js
@@ -1,13 +1,14 @@
 const Clarifai = require('clarifai'); 
 
 const app = new Clarifai.App({
-    apiKey: process.env.API_CLARIFAI /* The key is a config var - Environment variable onHeroky settings */
+    apiKey: '6b3500d33e07448f97c71d3951597925' /* The key is a config var - Environment variable onHeroky settings */
   });
-
+// will try '53e1df302c079b3db8a0a36033ed2d15' instead of Clarifai.FACE_DETECT_MODEL, as the latter is not working
 const handleApiCall = (req, res) => {
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    app.models.predict('53e1df302c079b3db8a0a36033ed2d15', req.body.input)
     .then(data => {
         res.json(data);
+        console.log(data); 
     })
     .catch(err => {
         res.status(400).json('unable to work with API')
--- server.js
+++ server.js
@@ -30,5 +30,5 @@ app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, sqldatab
 app.put('/image', (req, res) => { image.handleImage(req, res, sqldatabase) })
 app.post('/imageUrl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(process.env.PORT || 3000, () => { console.log(`app is running on port ${process.env.PORT}`) })
+app.listen(process.env.PORT || 3001, () => { console.log(`app is running on port ${process.env.PORT}`) })
 
