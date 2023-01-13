efd3357fdc43822fbc7c4e81aca4c02c694678c7
Zabar Ullah <60830173+zabarullah@users.noreply.github.com>
2023-01-13 16:48:25 +0000

    Update image.js
    Added api manually as there was an issue connecting to the Clarifai api with process.env.API_CLARIFAI

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai'); 
 
 const app = new Clarifai.App({
-    apiKey: process.env.API_CLARIFAI /* The key is a config var - Environment variable onHeroky settings */
+    apiKey: 'http://localhost:3000' /* The key is a config var - Environment variable onHeroky settings */
   });
 
 const handleApiCall = (req, res) => {
